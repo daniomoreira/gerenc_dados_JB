3297ebd06aaf8c0cbe783183e91fe367244b553f
Danielle Moreira <daniomoreira@gmail.com>
2020-02-11 17:01:29 -0300

    adiciona dados limpos, primeiro script e metadados

---
+++ R/0_ler-tabela.R
@@ -0,0 +1,20 @@
+
+# Script para ler tabelas
+
+## os dados originais estão em ex_04_original.xlxs
+
+# Para descobrir a pasta de trabalho
+getwd()
+
+# Para definir a pasta de trabalho (se precisar, pois aqui, não é necessário)
+# setwd("./data")
+
+
+# Para ler uma tabela csv
+
+?read.table
+# ou
+help(read.table)
+
+Tab_medidas <- read.csv ("./data/ex_04_formatado.csv", header = T, sep = ";")
+Tab_medidas
